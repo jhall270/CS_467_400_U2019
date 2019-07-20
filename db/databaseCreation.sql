860ab460255543eece9f4a7863693d2cc1fa1eb2
chadfingerson@gmail.com <chadfingerson@users.noreply.github.com>
2019-07-20 15:55:40 -0700

    Updated database for auth0 userid

--- db/databaseCreation.sql
+++ db/databaseCreation.sql
@@ -2,6 +2,7 @@
 CREATE TABLE IF NOT EXISTS User (
   Id INTEGER PRIMARY KEY AUTOINCREMENT,
   UserName varchar(64) UNIQUE,
+  Email varchar(64) UNIQUE,
   FirstName varchar(64) NOT NULL,
   LastName varchar(64) NOT NULL,
   TimeStamp datetime DEFAULT CURRENT_TIMESTAMP,
@@ -11,12 +12,12 @@ CREATE TABLE IF NOT EXISTS User (
  );
 
 
-INSERT INTO User (UserName, FirstName, LastName, TimeStamp, Signature, SoftDelete, IsAdmin) VALUES
-('user1@user.com', 'UserOneFirstName', 'UserOneLastName', '2019-07-11 14:46:55', "", '0', '0'),
-('user2@user.com', 'UserTwoFirstName', 'UserTwoLastName', '2019-07-11 14:46:55', "", '0', '0'),
-('user3@user.com', 'UserThreeFirstName', 'UserThreeLastName', '2019-07-11 14:46:55', "", '0', '0'),
-('admin1@admin.com', 'AdminOneFirstName', 'AdminOneLastName', '2019-07-11 14:46:55', "", '0', '1'),
-('admin2@admin.com', 'AdminTwoLastName', 'AdminTwoLastName', '2019-07-11 14:46:55', "", '0', '1');
+INSERT INTO User (UserName, Email, FirstName, LastName, TimeStamp, Signature, SoftDelete, IsAdmin) VALUES
+('auth0|5d2a50668935cd0e8a322637', 'user1@user.com', 'UserOneFirstName', 'UserOneLastName', '2019-07-11 14:46:55', "", '0', '0'),
+('auth0|5d2a50908935cd0e8a322639', 'user2@user.com', 'UserTwoFirstName', 'UserTwoLastName', '2019-07-11 14:46:55', "", '0', '0'),
+('auth0|5d2a50ac5171f20e7057b9c8', 'user3@user.com', 'UserThreeFirstName', 'UserThreeLastName', '2019-07-11 14:46:55', "", '0', '0'),
+('auth0|5d2a50c5ba4a7e0d2be7de96', 'admin1@admin.com', 'AdminOneFirstName', 'AdminOneLastName', '2019-07-11 14:46:55', "", '0', '1'),
+('auth0|5d2a50dba6d2ce0e4497e1d4', 'admin2@admin.com', 'AdminTwoLastName', 'AdminTwoLastName', '2019-07-11 14:46:55', "", '0', '1');
 
 
 CREATE TABLE IF NOT EXISTS Award (
@@ -33,6 +34,6 @@ CREATE TABLE IF NOT EXISTS Award (
 
   
 INSERT INTO Award (TypeOfAward, NameOfAwardee, EmailAddress, DateTimeAward, Department, UserName, SoftDelete) VALUES
-('EmployeeOfMonth', 'Bob Smith', '', '2019-07-01 17:47:56', 'Sales', 'user1@user.com', 0),
-('EmployeeOfWeek', 'Jane Doe', 'jane@doe.com', '2019-07-11 17:47:52', 'Engineering', 'user2@user.com', 0);
+('EmployeeOfMonth', 'Bob Smith', '', '2019-07-01 17:47:56', 'Sales', 'auth0|5d2a50668935cd0e8a322637', 0),
+('EmployeeOfWeek', 'Jane Doe', 'jane@doe.com', '2019-07-11 17:47:52', 'Engineering', 'auth0|5d2a50908935cd0e8a322639', 0);
   
